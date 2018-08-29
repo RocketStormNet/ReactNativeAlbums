261465cf8aa434430699749cf69d5d3b2af113f5
RocketStormNet <roketstorm@Mac-mini-Artur.local>
2018-08-29 15:20:13 +0500

    Added button and linking

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-    const { thumbnail_image, title, artist, image } = album;
+    const { thumbnail_image, title, artist, image, url } = album;
     const { 
         thumbnailStyle, 
         headerContentStyle, 
@@ -24,8 +25,15 @@ const AlbumDetail = ({album}) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => {Linking.openURL(url)}}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
